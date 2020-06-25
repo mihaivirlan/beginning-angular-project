4774dc7874d1b996531702ef34d5af4e89000600
mihaivirlan <mihaivirlan93@gmail.com>
2020-06-25 12:58:26 +0300

    Understanding Services & Dependency Injection

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -1,30 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { ProductsService } from './products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
   productName = 'A Book';
   isDisabled = true;
-  products = ['A Book', 'A Tree'];
+  products = [];
+  private productSubscription: Subscription;
 
-  constructor() {
+  constructor(private productsService: ProductsService) {
     setTimeout(() => {
       // this.productName = 'A Tree';
       this.isDisabled = false;
     }, 3000);
   }
 
+  ngOnInit() {
+    this.products = this.productsService.getProducts();
+    this.productSubscription = this.productsService.productsUpdated.subscribe(() => {
+      this.products = this.productsService.getProducts();
+    });
+  }
+
   onAddProduct(form) {
     // this.products.push(this.productName);
     if (form.valid) {
-      this.products.push(form.value.productName);
+      // this.products.push(form.value.productName);
+      this.productsService.addProduct(form.value.productName);
     }
   }
 
   onRemoveProduct(productName: string) {
     this.products = this.products.filter(p => p !== productName);
+  }
+
+  ngOnDestroy() {
+    this.productSubscription.unsubscribe();
   }
 
 }
